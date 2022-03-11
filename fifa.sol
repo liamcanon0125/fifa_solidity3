54c04515f08febfffeabe167b1f1d2d914f0e208
unknown <Panda202295@outlook.com>
2022-03-11 12:00:35 -0500

    Delete an element from Solidity array in order/out of order

---
+++ fifa.sol
@@ -0,0 +1,21 @@
+
+pragma solidity ^0.8.7;
+contract Contract{
+    string [] public data;
+     constructor() public{
+         data.push("John");
+         data.push("Bruce");
+         data.push("Tom");
+         data.push("Bart");
+         data.push("Cherry");
+     }
+
+     function removeNoOrder(uint index) external{
+        data[index] = data[data.length-1];
+        data.pop();
+     }
+     function removeInOrder(uint index) external{
+         for(uint i = index; i < data.length - 1; i++)
+         data[i] = data[i+1];
+     } data.pop();
+}
